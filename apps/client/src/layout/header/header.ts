48a2d06b380627143f2cd3e8f961d06a6905e086
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-11 19:05:19 +0300

    🪾 header_shape => 🛠️ defined base shape header

--- apps/client/src/layout/header/header.ts
+++ apps/client/src/layout/header/header.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SvgFillGhost } from '../../common/components/svgs/fill/ghost/ghost';
 import { SvgStrokeUserWrite } from '../../common/components/svgs/stroke/user_write/user-write';
+import { RouterLink } from '@angular/router';
+import { SvgStrokeBurger } from '../../common/components/svgs/stroke/burger/burger';
 
 @Component({
   selector: 'app-header',
-  imports: [SvgFillGhost, SvgStrokeUserWrite],
+  imports: [SvgFillGhost, SvgStrokeUserWrite, RouterLink, SvgStrokeBurger],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
