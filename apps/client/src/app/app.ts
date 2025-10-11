ff47ee5e63f297979b4af1542dca95c839840362
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-11 18:13:52 +0300

    🪾 main => 🛠️ _

--- apps/client/src/app/app.ts
+++ apps/client/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../layout/header/header';
-import { SvgAdvComplex } from '../common/components/svgs/advanced/complex/complex';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, SvgAdvComplex],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
 })
 export class App {}
