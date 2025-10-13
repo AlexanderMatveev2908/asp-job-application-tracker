8b400a406939920c295217ac91ac5def0a185dda
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-13 12:14:41 +0300

    🪾 base_shadow_btn => 🎨 patch link shadow

--- apps/client/src/common/components/links/link_shadow/link-shadow.ts
+++ apps/client/src/common/components/links/link_shadow/link-shadow.ts
@@ -1,13 +1,13 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgComponentOutlet } from '@angular/common';
+import { NgComponentOutlet, NgTemplateOutlet } from '@angular/common';
 import { LinkShadowConfT } from './etc/types';
 import { AppEventMeta } from '@/common/types/events';
 import { UseAppEventsSvc } from '@/core/hooks/use_app_events';
 
 @Component({
   selector: 'app-link-shadow',
-  imports: [RouterLink, NgComponentOutlet],
+  imports: [RouterLink, NgComponentOutlet, NgTemplateOutlet],
   templateUrl: './link-shadow.html',
   styleUrl: './link-shadow.scss',
 })
@@ -19,4 +19,5 @@ export class LinkShadow {
   public readonly metaEvent = computed(
     (): AppEventMeta => this.useAppEvents.getByT(this.conf().eventT)
   );
+  public readonly isExternal = computed(() => /^(https?:\/\/|mailto:|tel:)/.test(this.conf().path));
 }
