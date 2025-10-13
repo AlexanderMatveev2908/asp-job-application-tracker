e1de6c5c9b78d813d4fb2b1bd18032a09ae8f29c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-13 14:28:08 +0300

    🪾 base_shadow_btn => 🛠️ built base shadow btn

--- apps/client/src/pages/home/home.ts
+++ apps/client/src/pages/home/home.ts
@@ -1,10 +1,23 @@
+import { BtnShadow } from '@/common/components/btns/btn_shadow/btn-shadow';
+import { BtnShadowConfT } from '@/common/components/btns/btn_shadow/etc/types';
+import { SvgFillBash } from '@/common/components/svgs/fill/bash/bash';
 import { WrapPage } from '@/common/components/wrappers/wrap_page/wrap-page';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [WrapPage],
+  imports: [WrapPage, BtnShadow],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  public readonly confBtnScript = signal<BtnShadowConfT>({
+    label: 'Script worked 🎉',
+    Svg: SvgFillBash,
+    eventT: 'INFO',
+    meta: {
+      isDisabled: false,
+      isPending: false,
+    },
+  });
+}
