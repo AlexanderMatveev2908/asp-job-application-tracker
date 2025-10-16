eabab5480b618b4c452dae6956001459d1b89e03
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-17 00:32:10 +0300

    🪾 header_drop => 🛠️ defined shape using span as inner sub component

--- apps/client/src/common/components/drop/abs/drop-abs.ts
+++ apps/client/src/common/components/drop/abs/drop-abs.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import { SpanPropsT, SpanSizesPropsT } from '../../els/span/etc/types';
+import { Span } from '../../els/span/span';
 
 @Component({
   selector: 'app-drop-abs',
-  imports: [],
+  imports: [Span],
   templateUrl: './drop-abs.html',
   styleUrl: './drop-abs.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DropAbs {}
+export class DropAbs {
+  public readonly isOpen: InputSignal<boolean> = input.required();
+  public readonly handleToggle: InputSignal<() => void> = input.required();
+  public readonly spanProps: InputSignal<SpanPropsT> = input.required();
+  public readonly spanSizesProps: InputSignal<Partial<SpanSizesPropsT>> = input.required();
+}
--- apps/client/src/layout/header/header.ts
+++ apps/client/src/layout/header/header.ts
@@ -1,24 +1,46 @@
-import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgClass } from '@angular/common';
 import { SvgFillGhost } from '@/common/components/svgs/fill/ghost/ghost';
 import { SvgStrokeUserWrite } from '@/common/components/svgs/stroke/user_write/user-write';
 import { SvgStrokeBurger } from '@/common/components/svgs/stroke/burger/burger';
 import { SvgFillClose } from '@/common/components/svgs/fill/close/close';
 import { SidebarSlice } from '@/features/sidebar/slice';
+import { DropAbs } from '@/common/components/drop/abs/drop-abs';
+import { SpanPropsT, SpanSizesPropsT } from '@/common/components/els/span/etc/types';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [SvgFillGhost, SvgStrokeUserWrite, RouterLink, SvgStrokeBurger, SvgFillClose, NgClass],
+  imports: [SvgFillGhost, RouterLink, DropAbs, SvgFillClose, SvgStrokeBurger, NgClass],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
   public readonly sideSlice: SidebarSlice = inject(SidebarSlice);
-  public readonly isOpen: Signal<boolean> = computed(() => this.sideSlice.sideState().isOpen);
+  public readonly isSideOpen: Signal<boolean> = computed(() => this.sideSlice.sideState().isOpen);
+  public readonly isDropOpen: WritableSignal<boolean> = signal(false);
 
-  public handleToggle(): void {
+  public readonly toggleSide: () => void = () => {
     this.sideSlice.toggle();
-  }
+  };
+  public readonly toggleDrop: () => void = () => {
+    this.isDropOpen.set(!this.isDropOpen);
+  };
+
+  public readonly spanProps: Signal<SpanPropsT> = computed(() => ({
+    label: null,
+    Svg: SvgStrokeUserWrite,
+  }));
+  public readonly spanSizesProps: Partial<SpanSizesPropsT> = {
+    svg: '3xl',
+  };
 }
