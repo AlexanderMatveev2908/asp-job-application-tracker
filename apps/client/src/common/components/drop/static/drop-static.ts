aa041c05e0e06045a47fdad1cbcc2313e82c96ec
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-16 22:10:25 +0300

    🪾 side_bar_drop_menu => 🎨 _

--- apps/client/src/common/components/drop/static/drop-static.ts
+++ apps/client/src/common/components/drop/static/drop-static.ts
@@ -16,10 +16,10 @@ import {
 import { SpanPropsT, SpanSizesPropsT } from '../../els/span/etc/types';
 import { Span } from '../../els/span/span';
 import { SvgFillUp } from '../../svgs/fill/up/up';
-import { Lorem } from '@/core/lib/etc';
 import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { ElDomT, RefDomT } from '@/common/types/etc';
 import { RecTwdClsDropT } from '../etc/types';
+import { LibDrop } from '../etc/lib_drop';
 
 @Component({
   selector: 'app-drop-static',
@@ -28,7 +28,7 @@ import { RecTwdClsDropT } from '../etc/types';
   styleUrl: './drop-static.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DropStatic extends Lorem implements AfterViewInit, AfterContentChecked {
+export class DropStatic implements AfterViewInit, AfterContentChecked {
   public readonly spanProps: InputSignal<SpanPropsT> = input.required();
   public readonly isOpen: WritableSignal<boolean> = signal(false);
   public readonly spanSizesProps: SpanSizesPropsT = {
@@ -41,16 +41,12 @@ export class DropStatic extends Lorem implements AfterViewInit, AfterContentChec
   @ContentChild('dropContent', { read: TemplateRef }) dropContentRef!: TemplateRef<unknown>;
 
   public readonly twd: Signal<RecTwdClsDropT> = computed(() => {
-    const isOp: boolean = this.isOpen();
-    return {
-      root: isOp ? 'border-blue-600 text-blue-600' : 'border-gray-300 text-gray-300',
-      arrow: isOp ? 'rotate-180' : 'rotate-0',
-      wrapContent: isOp ? 'opacity-1 translate-y-0' : 'opacity-0 translate-y-[-15px]',
-    };
+    const isDropOpen: boolean = this.isOpen();
+    return LibDrop.getTwdCls(isDropOpen);
   });
 
   public readonly maxH: Signal<string> = computed(() =>
-    this.isOpen() && this.wrapperH ? `${this.wrapperH()}px` : '0px'
+    this.isOpen() ? `${this.wrapperH()}px` : '0px'
   );
 
   public onClick(): void {
