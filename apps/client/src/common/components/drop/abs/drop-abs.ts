53a47bd85c1d2fcb94b48d05bd9015e3bf8d5bbd
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-17 11:06:43 +0300

    🪾 header_drop => 🛠️ defined n mouse out e for drop abs

--- apps/client/src/common/components/drop/abs/drop-abs.ts
+++ apps/client/src/common/components/drop/abs/drop-abs.ts
@@ -1,17 +1,48 @@
-import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  ContentChild,
+  HostListener,
+  input,
+  InputSignal,
+  Signal,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { SpanPropsT, SpanSizesPropsT } from '../../els/span/etc/types';
 import { Span } from '../../els/span/span';
+import { NgTemplateOutlet, NgClass } from '@angular/common';
+import { ElDomT, RefDomT } from '@/common/types/etc';
 
 @Component({
   selector: 'app-drop-abs',
-  imports: [Span],
+  imports: [Span, NgTemplateOutlet, NgClass],
   templateUrl: './drop-abs.html',
   styleUrl: './drop-abs.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DropAbs {
   public readonly isOpen: InputSignal<boolean> = input.required();
-  public readonly handleToggle: InputSignal<() => void> = input.required();
+  public readonly setIsDropOpen: InputSignal<(val: boolean) => void> = input.required();
   public readonly spanProps: InputSignal<SpanPropsT> = input.required();
   public readonly spanSizesProps: InputSignal<Partial<SpanSizesPropsT>> = input.required();
+
+  public readonly translation: Signal<string> = computed(() =>
+    this.isOpen()
+      ? 'translate-y-[0%] opacity-1'
+      : 'translate-y-[-50%] pointer-events-none opacity-0'
+  );
+
+  @ViewChild('drop') drop: RefDomT;
+  @ContentChild('dropContent', { read: TemplateRef }) dropContentRef!: TemplateRef<unknown>;
+
+  @HostListener('document:mousedown', ['$event'])
+  public onMouseDown(e: MouseEvent): void {
+    const elDOM: ElDomT = this.drop?.nativeElement;
+    const target: HTMLElement = e.target as HTMLElement;
+    if (!elDOM) return;
+
+    if (!elDOM.contains(e.target as HTMLLIElement)) this.setIsDropOpen()(false);
+  }
 }
--- apps/client/src/layout/header/header.ts
+++ apps/client/src/layout/header/header.ts
@@ -16,15 +16,20 @@ import { SidebarSlice } from '@/features/sidebar/slice';
 import { DropAbs } from '@/common/components/drop/abs/drop-abs';
 import { SpanPropsT, SpanSizesPropsT } from '@/common/components/els/span/etc/types';
 import { NgClass } from '@angular/common';
+import { LinksSvc } from '@/core/ui_factory/links';
+import { LinkT } from '@/common/types/links';
+import { Span } from '@/common/components/els/span/span';
 
 @Component({
   selector: 'app-header',
-  imports: [SvgFillGhost, RouterLink, DropAbs, SvgFillClose, SvgStrokeBurger, NgClass],
+  imports: [SvgFillGhost, RouterLink, DropAbs, SvgFillClose, SvgStrokeBurger, NgClass, Span],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
+  private readonly linksSvc: LinksSvc = inject(LinksSvc);
+
   public readonly sideSlice: SidebarSlice = inject(SidebarSlice);
   public readonly isSideOpen: Signal<boolean> = computed(() => this.sideSlice.sideState().isOpen);
   public readonly isDropOpen: WritableSignal<boolean> = signal(false);
@@ -32,8 +37,14 @@ export class Header {
   public readonly toggleSide: () => void = () => {
     this.sideSlice.toggle();
   };
-  public readonly toggleDrop: () => void = () => {
-    this.isDropOpen.set(!this.isDropOpen);
+  public readonly setIsDropOpen: (val: boolean) => void = (val: boolean) => {
+    this.isDropOpen.set(val);
+  };
+
+  public readonly notLoggedLinks: LinkT[] = this.linksSvc.notLogged;
+  public readonly spanSizesDrop: SpanSizesPropsT = {
+    svg: 'md',
+    txt: 'lg',
   };
 
   public readonly spanProps: Signal<SpanPropsT> = computed(() => ({
