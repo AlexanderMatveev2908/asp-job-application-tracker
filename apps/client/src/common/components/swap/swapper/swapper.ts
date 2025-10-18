7c2285101931e9e27dac0145e5b571d7dbfa67f7
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-18 17:49:15 +0300

    🪾 form_slider => 🛠️ made max height become reactive

--- apps/client/src/common/components/swap/swapper/swapper.ts
+++ apps/client/src/common/components/swap/swapper/swapper.ts
@@ -1,12 +1,23 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   computed,
+  ContentChildren,
+  effect,
+  EffectRef,
+  inject,
   input,
   InputSignal,
+  QueryList,
+  signal,
   Signal,
+  WritableSignal,
 } from '@angular/core';
 import { SwapBtns } from './swap_btns/swap-btns';
+import { ElDomT, RefDomT } from '@/common/types/etc';
+import { ErrApp } from '@/core/lib/err';
+import { UsePlatformSvc } from '@/core/hooks/use_platform';
 
 @Component({
   selector: 'app-swapper',
@@ -15,11 +26,57 @@ import { SwapBtns } from './swap_btns/swap-btns';
   styleUrl: './swapper.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Swapper {
+export class Swapper implements AfterViewInit {
+  private readonly usePlatform: UsePlatformSvc = inject(UsePlatformSvc);
   public readonly swap: InputSignal<number> = input.required();
   public readonly setSwap: InputSignal<(val: number) => void> = input.required();
   public readonly maxSwaps: InputSignal<number> = input.required();
 
+  public readonly maxH: WritableSignal<string | null> = signal(null);
+
   // eslint-disable-next-line no-magic-numbers
   public readonly transform: Signal<string> = computed(() => `-${this.swap() * 100}%`);
+
+  @ContentChildren('swapDiv', { descendants: false })
+  private readonly projectedEls: QueryList<RefDomT> | undefined;
+
+  private calcH(): void {
+    if (!this.projectedEls) return;
+
+    const maxSwaps: number = this.maxSwaps();
+
+    const swapDivDOM: HTMLElement[] = this.projectedEls
+      .map((el: RefDomT) => el?.nativeElement)
+      .filter((el: ElDomT) => !!el && el?.tagName === 'DIV') as HTMLElement[];
+
+    if (swapDivDOM.length !== maxSwaps)
+      throw new ErrApp(`expected ${maxSwaps} divs, received ${swapDivDOM}`);
+
+    let idx: number = 0;
+
+    do {
+      if (idx !== this.swap()) {
+        idx++;
+        continue;
+      }
+
+      const curr: HTMLElement = swapDivDOM[idx];
+      this.maxH.set(`${curr.scrollHeight}px`);
+      break;
+    } while (idx < maxSwaps);
+  }
+
+  ngAfterViewInit(): void {
+    this.usePlatform.whenDomPainted(() => {
+      this.calcH();
+    });
+  }
+
+  public onSwapChange: EffectRef = effect(() => {
+    void this.swap();
+
+    setTimeout(() => {
+      this.calcH();
+    }, 0);
+  });
 }
