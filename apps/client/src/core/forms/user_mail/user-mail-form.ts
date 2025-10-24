59a049778e226f522a6828dd5c1ceb717ff48065
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-24 23:25:40 +0300

    🪾 manage_account => 🛠️ built change_mail_form

--- apps/client/src/core/forms/user_mail/user-mail-form.ts
+++ apps/client/src/core/forms/user_mail/user-mail-form.ts
@@ -1,12 +1,12 @@
 import { FormFieldTxt } from '@/common/components/forms/form_field_txt/form-field-txt';
 import { TxtFieldT } from '@/common/types/forms';
 import { UseKitFormSvc } from '@/core/hooks/kits/kit_form/0.use_kit_form';
-import { MailFormMng } from '@/core/paperwork/etc/mail';
+import { MailFormMng, MailFormT } from '@/core/paperwork/etc/mail';
 import { ApiTrackerSvc } from '@/core/store/api/etc/tracker';
 import { MailFormUiFkt } from '@/core/ui_fkt/form_fields/1.mail';
 import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { EMPTY } from 'rxjs';
+import { Observable } from 'rxjs';
 import { FormShape } from '@/common/components/forms/form_shape/form-shape';
 
 @Component({
@@ -20,12 +20,15 @@ import { FormShape } from '@/common/components/forms/form_shape/form-shape';
 export class UserMailForm extends UseKitFormSvc {
   // ? props
   public readonly testId: InputSignal<string> = input.required();
+  public readonly strategy: InputSignal<(data: MailFormT) => Observable<unknown>> =
+    input.required();
 
+  // ? static assets
   public readonly form: FormGroup = MailFormMng.form();
   public readonly mailField: TxtFieldT = MailFormUiFkt.mailField();
   public readonly ctrl: FormControl = this.getCtrl('email');
 
   public readonly onSubmit: () => void = () => {
-    this.submitForm((_: unknown) => EMPTY);
+    this.submitForm((data: unknown) => this.strategy()(data as MailFormT));
   };
 }
--- apps/client/src/pages/user/manage_account/swaps/change_mail_form/change-mail-form.ts
+++ apps/client/src/pages/user/manage_account/swaps/change_mail_form/change-mail-form.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UserMailForm } from '@/core/forms/user_mail/user-mail-form';
+import { MailFormT } from '@/core/paperwork/etc/mail';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-change-mail-form',
@@ -8,4 +10,10 @@ import { UserMailForm } from '@/core/forms/user_mail/user-mail-form';
   styleUrl: './change-mail-form.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChangeMailForm {}
+export class ChangeMailForm {
+  public readonly strategy: (data: MailFormT) => Observable<unknown> = (data: MailFormT) => {
+    console.log(data);
+
+    return of(data);
+  };
+}
