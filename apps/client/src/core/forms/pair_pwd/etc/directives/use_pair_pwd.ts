c8dd60630230a50089f3beec3bd2a3bfd3cae739
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-24 14:04:32 +0300

    🪾 manage_account => 🛠️ refactored code following DRY principle

--- apps/client/src/core/forms/pair_pwd/etc/directives/use_pair_pwd.ts
+++ apps/client/src/core/forms/pair_pwd/etc/directives/use_pair_pwd.ts
@@ -1,11 +1,9 @@
 import { Directive, inject } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { PairPwdFormMng } from '../paperwork/form_mng';
-import { UseKitFormSvc } from '@/core/hooks/kits/kit_form/0.use_kit_form';
 import { CbcHmacSlice } from '@/features/cbcHmac/slice';
+import { UseNoticeKitSvc } from '@/features/notice/etc/use_notice_kit';
 
 @Directive()
-export abstract class UsePairPwfFormDir extends UseKitFormSvc {
+export abstract class UsePairPwfFormDir {
   protected readonly cbcHmacSlice: CbcHmacSlice = inject(CbcHmacSlice);
-  public readonly form: FormGroup = PairPwdFormMng.form();
+  protected readonly useNoticeKit: UseNoticeKitSvc = inject(UseNoticeKitSvc);
 }
--- apps/client/src/pages/auth/recover_pwd/recover-pwd.ts
+++ apps/client/src/pages/auth/recover_pwd/recover-pwd.ts
@@ -7,15 +7,14 @@ import { UseAuthKitSvc } from '@/features/auth/etc/use_auth_kit';
 import { Nullable } from '@/common/types/etc';
 import { ErrApp } from '@/core/lib/err';
 import { PairPwdFormT } from '@/core/forms/pair_pwd/etc/paperwork/form_mng';
-import { catchError, EMPTY, tap, throwError } from 'rxjs';
+import { catchError, EMPTY, Observable, tap, throwError } from 'rxjs';
 import { ErrApiT, ResApiT, StatusT } from '@/core/store/api/etc/types';
 import { JwtResT } from '@/features/auth/etc/types';
-import { FormShape } from '@/common/components/forms/form_shape/form-shape';
 import { UseRouteMngSvc } from '@/core/hooks/use_route_mng';
 
 @Component({
   selector: 'app-recover-pwd',
-  imports: [CsrWithTitle, FormShape, FormPairPwd],
+  imports: [CsrWithTitle, FormPairPwd],
   templateUrl: './recover-pwd.html',
   styleUrl: './recover-pwd.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,41 +24,45 @@ export class RecoverPwd extends UsePairPwfFormDir implements OnInit {
   private readonly routerProtection: UseRouteMngSvc = inject(UseRouteMngSvc);
   private readonly useAuthKit: UseAuthKitSvc = inject(UseAuthKitSvc);
 
-  public readonly onSubmit: () => void = () => {
+  public readonly strategy: (data: PairPwdFormT) => Observable<unknown> = (data: PairPwdFormT) => {
     const cbcHmacToken: Nullable<string> = this.cbcHmacSlice.cbcHmac();
-    if (!cbcHmacToken) throw new ErrApp('bug cbc hmac submit form recover pwd');
-
-    this.submitForm((data: unknown) =>
-      this.useAuthKit.authApi
-        .recoverPwd({
-          cbcHmacToken,
-          password: (data as PairPwdFormT).password,
-        })
-        .pipe(
-          tap((res: ResApiT<JwtResT>) => {
-            this.useAuthKit.authSlice.login(res.accessToken, { withTmr: true });
-            this.cbcHmacSlice.clearCbcHmac();
-
-            this.useNoticeKit.pushNotice({
-              eventT: 'OK',
-              msg: 'Password updated',
-              status: 200,
-            });
-          }),
-          catchError((err: ErrApiT<void>) => {
-            if (err.status !== StatusT.UNAUTHORIZED) return throwError(() => err);
-
-            this.cbcHmacSlice.clearCbcHmac();
-            this.useNoticeKit.pushNotice({
-              eventT: 'ERR',
-              msg: err.error?.msg ?? 'Token invalid',
-              status: 401,
-            });
-
-            return EMPTY;
-          })
+    return !cbcHmacToken
+      ? throwError(
+          () =>
+            new ErrApp(
+              'bug => missing cbc_hmac and still user submit form recover_pwd',
+              StatusT.UNAUTHORIZED
+            )
         )
-    );
+      : this.useAuthKit.authApi
+          .recoverPwd({
+            cbcHmacToken,
+            password: data.password,
+          })
+          .pipe(
+            tap((res: ResApiT<JwtResT>) => {
+              this.useAuthKit.authSlice.login(res.accessToken, { withTmr: true });
+              this.cbcHmacSlice.clearCbcHmac();
+
+              this.useNoticeKit.pushNotice({
+                eventT: 'OK',
+                msg: 'Password updated',
+                status: 200,
+              });
+            }),
+            catchError((err: ErrApiT<void>) => {
+              if (err.status !== StatusT.UNAUTHORIZED) return throwError(() => err);
+
+              this.cbcHmacSlice.clearCbcHmac();
+              this.useNoticeKit.pushNotice({
+                eventT: 'ERR',
+                msg: err.error?.msg ?? 'Token invalid',
+                status: 401,
+              });
+
+              return EMPTY;
+            })
+          );
   };
 
   ngOnInit(): void {
