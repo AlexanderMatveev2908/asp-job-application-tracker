4ed264833e44e4f2323dd5f17612647b47780b23
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-25 11:05:38 +0300

    🪾 manage_account_forms_shape => 🛠️ finsihed shapes, can start lgoic

--- apps/client/src/pages/user/manage_account/swaps/setup_2fa/setup-2fa.ts
+++ apps/client/src/pages/user/manage_account/swaps/setup_2fa/setup-2fa.ts
@@ -1,10 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BtnShadow } from '@/common/components/btns/btn_shadow/btn-shadow';
+import { SpanEventPropsT } from '@/common/components/els/span/etc/types';
 
 @Component({
   selector: 'app-setup-2fa',
-  imports: [],
+  imports: [BtnShadow],
   templateUrl: './setup-2fa.html',
   styleUrl: './setup-2fa.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Setup2fa {}
+export class Setup2fa {
+  public readonly spanProps: SpanEventPropsT = {
+    eventT: 'OK',
+    label: 'Setup',
+    Svg: null,
+  };
+}
