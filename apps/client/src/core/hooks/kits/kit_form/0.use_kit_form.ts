6009de51a4360865b20e38cfa571084846f72d8c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-25 19:34:09 +0300

    🪾 verify_mail_user => 🛠️ _

--- apps/client/src/core/hooks/kits/kit_form/0.use_kit_form.ts
+++ apps/client/src/core/hooks/kits/kit_form/0.use_kit_form.ts
@@ -1,31 +1,9 @@
-import { inject, Injectable } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { UseApiTrackerHk } from '@/core/store/api/etc/hooks/use_tracker';
-import { ZodCheck } from '@/core/paperwork/zod_check';
-import { UseKitSideApiSvc } from '@/core/services/use_kit_side_api';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { UseKitFormRootHk } from './0.use_kit_form_root';
 
 @Injectable()
-export abstract class UseKitFormHk {
-  // ? expected to be present
-  public abstract form: FormGroup;
-
-  // ? svc
-  public readonly apiTracker: UseApiTrackerHk = inject(UseApiTrackerHk);
-  protected readonly useSideApiKit: UseKitSideApiSvc = inject(UseKitSideApiSvc);
-
-  // ? helpers
-  public readonly getCtrl: (name: string) => FormControl = (name: string) =>
-    this.form.get(name) as FormControl;
-
-  protected readonly submitForm: (cb: (data: unknown) => Observable<unknown>) => void = (
-    cb: (data: unknown) => Observable<unknown>
-  ) => {
-    if (!this.form.valid) {
-      ZodCheck.onSubmitFailed(this.form);
-      return;
-    }
-
-    this.apiTracker.track(cb(this.form.value)).subscribe();
-  };
+export abstract class UseKitFormHk extends UseKitFormRootHk {
+  public abstract readonly form: FormGroup;
+  protected readonly getForm: () => FormGroup = () => this.form;
 }
