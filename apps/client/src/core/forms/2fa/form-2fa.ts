6e57c9c27585e6ba9bba97f500b1cba711b65166
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-26 23:28:38 +0200

    🪾 login_2fa => 🛠️ _

--- apps/client/src/core/forms/2fa/form-2fa.ts
+++ apps/client/src/core/forms/2fa/form-2fa.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CsrWithTitle } from '@/common/components/hoc/page/csr_with_title/csr-with-title';
 import { Swapper } from '@/common/components/swap/swapper/swapper';
 import { UseSwapHk } from '@/core/hooks/use_swap/use_swap';
+import { SwapItem } from '@/common/components/swap/swap_item/swap-item';
 
 @Component({
   selector: 'app-form-2fa',
-  imports: [CsrWithTitle, Swapper],
+  imports: [CsrWithTitle, Swapper, SwapItem],
   templateUrl: './form-2fa.html',
   styleUrl: './form-2fa.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
