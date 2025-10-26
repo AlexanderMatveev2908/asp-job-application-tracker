69a77ac2649f7911b6cbe2472c0bbbad6ac09305
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-26 19:49:42 +0200

    🪾 setup_2fa => 🛠️ _

--- apps/client/src/pages/user/manage_account/swap_forms/setup_2fa/setup-2fa.ts
+++ apps/client/src/pages/user/manage_account/swap_forms/setup_2fa/setup-2fa.ts
@@ -2,6 +2,8 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
+  input,
+  InputSignal,
   signal,
   Signal,
   WritableSignal,
@@ -10,7 +12,7 @@ import { UseApiTrackerHk } from '@/core/store/api/etc/hooks/use_tracker';
 import { Nullable } from '@/common/types/etc';
 import { Setup2faReturnT, UserT } from '@/features/user/etc/types';
 import { LibApiShape } from '@/core/store/api/etc/lib/shape';
-import { tap } from 'rxjs';
+import { finalize, tap } from 'rxjs';
 import { ResApiT } from '@/core/store/api/etc/types';
 import { UseKitFormUserSvc } from '@/features/user/etc/services/use_kit_form_user';
 import { UseMetaEventDir } from '@/core/directives/use_meta_event';
@@ -26,6 +28,9 @@ import { ContentSetup2fa } from './content_options/setup/content-setup-2fa';
   providers: [UseApiTrackerHk],
 })
 export class Setup2fa {
+  // ? props
+  public readonly triggerCalcH: InputSignal<() => void> = input.required();
+
   public readonly resSetup2FA: WritableSignal<Nullable<Setup2faReturnT>> = signal(null);
   // ? scv
   private readonly useKitFormUser: UseKitFormUserSvc = inject(UseKitFormUserSvc);
@@ -48,7 +53,8 @@ export class Setup2fa {
         this.useKitFormUser.useKitUser.userApi.setup2FA({ cbcHmacToken: cbcHmac! }).pipe(
           tap((res: ResApiT<Setup2faReturnT>) => {
             this.resSetup2FA.set(res);
-          })
+          }),
+          finalize(() => this.triggerCalcH()())
         )
       )
     ).subscribe();
