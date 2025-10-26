cff752564e5bdd5156f2e8f67d2b61cacaecaff8
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-26 23:26:24 +0200

    🪾 login_2fa => 🛠️ _

--- apps/client/src/pages/user/manage_account/manage-account.ts
+++ apps/client/src/pages/user/manage_account/manage-account.ts
@@ -14,18 +14,18 @@ import {
 import { CsrWithTitle } from '@/common/components/hoc/page/csr_with_title/csr-with-title';
 import { Swapper } from '@/common/components/swap/swapper/swapper';
 import { UseSwapHk } from '@/core/hooks/use_swap/use_swap';
-import { UserWrapSwap } from './components/user_wrap_swap/user-wrap-swap';
 import { ChangeMailForm } from './swap_forms/change_mail_form/change-mail-form';
 import { ChangePwdForm } from './swap_forms/change_pwd_form/change-pwd-form';
 import { Setup2fa } from './swap_forms/setup_2fa/setup-2fa';
 import { DeleteAccount } from './swap_forms/delete_account/delete-account';
+import { SwapItem } from '@/common/components/swap/swap_item/swap-item';
 
 @Component({
   selector: 'app-manage-account',
   imports: [
     CsrWithTitle,
     Swapper,
-    UserWrapSwap,
+    SwapItem,
     ChangeMailForm,
     ChangePwdForm,
     Setup2fa,
