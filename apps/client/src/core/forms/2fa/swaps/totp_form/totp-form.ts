89ca76bb9e7e0ec8408a05ff01c34d03b4b45388
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-27 13:18:36 +0200

    🪾 login_2fa => 🔥 _

--- apps/client/src/core/forms/2fa/swaps/totp_form/totp-form.ts
+++ apps/client/src/core/forms/2fa/swaps/totp_form/totp-form.ts
@@ -1,10 +1,17 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  OnInit,
+  Signal,
+} from '@angular/core';
 import { SwapItem } from '@/common/components/swap/swap_item/swap-item';
 import { TotpFormUiFkt, TotpPartFieldsT } from './etc/ui_fkt';
 import { FormSubmit } from '@/common/components/forms/form_submit/form-submit';
 import { UseIDsDir } from '@/core/directives/use_ids';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { TotpFormMng } from './etc/paperwork/form_mng';
+import { TotpFormMng, TotpFormT } from './etc/paperwork/form_mng';
 import { UseKitFormHk } from '@/core/hooks/kits/kit_form/0.use_kit_form';
 import { EMPTY } from 'rxjs';
 import { UseApiTrackerHk } from '@/core/store/api/etc/hooks/use_tracker';
@@ -14,6 +21,9 @@ import { FormFieldErr } from '@/common/components/forms/form_field_err/form-fiel
 
 import { TotpPart } from './totp_part/totp-part';
 import { UseFormFieldDir } from '@/core/directives/forms/form_field/0.use_form_field';
+import { Nullable } from '@/common/types/etc';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { LibTotpFormMeta } from './etc/lib/metadata';
 
 @Component({
   selector: 'app-totp-form',
@@ -31,7 +41,7 @@ import { UseFormFieldDir } from '@/core/directives/forms/form_field/0.use_form_f
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [UseApiTrackerHk, UseInjCtxHk],
 })
-export class TotpForm extends UseKitFormHk implements AfterViewInit {
+export class TotpForm extends UseKitFormHk implements OnInit, AfterViewInit {
   public readonly partsFields: TotpPartFieldsT[] = TotpFormUiFkt.partsFields();
 
   public readonly form: FormGroup = TotpFormMng.form();
@@ -49,9 +59,40 @@ export class TotpForm extends UseKitFormHk implements AfterViewInit {
     });
   };
 
+  private formVal: Nullable<Signal<TotpFormT>> = null;
+
+  ngOnInit(): void {
+    this.useInjCtx.inCtx(() => {
+      this.formVal = toSignal(this.form.valueChanges, {
+        initialValue: this.form.value,
+      });
+    });
+  }
+
   ngAfterViewInit(): void {
     this.useInjCtx.useDOM(() => {
       FocusDOM.byDataField('totp.0');
     });
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  public onKeysDown(e: KeyboardEvent): void {
+    const val: Nullable<string[]> = this.formVal?.()?.totp ?? null;
+    if (!val) return;
+
+    const key: string = e.key;
+
+    const { currFocus, currTotp, currIdx, allIn } = LibTotpFormMeta.main(val);
+
+    // ! allIn check all value are:
+    // ! number i where expected or truthy for DOM/strings
+    if (!allIn) return;
+
+    const ctrl: FormControl = this.getCtrl(currTotp!);
+
+    if (key === 'Backspace') {
+      ctrl.setValue('');
+      FocusDOM.byDataField(`totp.${currIdx! - 1}`);
+    }
   }
 }
