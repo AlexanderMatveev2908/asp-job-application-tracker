728fc39b1431cc8dfa51aa22484a7c6237e1e13e
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-28 23:18:41 +0200

    🪾 create_application => 🔥 _

--- apps/client/src/features/applications/etc/forms/job_application/etc/ui_fkt/index.ts
+++ apps/client/src/features/applications/etc/forms/job_application/etc/ui_fkt/index.ts
@@ -1,5 +1,7 @@
-import { TxtFieldT } from '@/common/types/forms';
+import { CheckBoxFieldT, TxtFieldT } from '@/common/types/forms';
+import { LibPrs } from '@/core/lib/data_structure/prs';
 import { FormFieldsUiFkt } from '@/core/ui_fkt/form_fields/0.root';
+import { ApplicationStatusT } from '@/features/applications/etc/types';
 
 export class ApplicationFormUiFkt extends FormFieldsUiFkt {
   public static txtInputs: () => TxtFieldT[] = () => [
@@ -8,4 +10,13 @@ export class ApplicationFormUiFkt extends FormFieldsUiFkt {
     this.txtFieldOf({ name: 'notes', type: 'textarea' }),
     this.txtFieldOf({ name: 'appliedAt', type: 'date' }),
   ];
+
+  public static readonly fieldsBox: () => CheckBoxFieldT[] = () =>
+    Object.values(ApplicationStatusT).map((v: string) =>
+      this.checkBoxFieldOf({
+        name: 'status',
+        val: v,
+        label: LibPrs.titleCase(v.split('_').join(' ')),
+      })
+    );
 }
--- apps/client/src/features/applications/etc/forms/job_application/statuses/application-statuses.ts
+++ apps/client/src/features/applications/etc/forms/job_application/statuses/application-statuses.ts
@@ -1,14 +1,15 @@
 import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
 import { DynamicSwapper } from '@/common/components/swap/dynamic_swapper/dynamic-swapper';
 import { UseDynamicSwapHk } from '@/core/hooks/swap/use_dynamic_swap';
-import { v4 } from 'uuid';
 import { CheckBoxFieldT } from '@/common/types/forms';
 import { UseIDsDir } from '@/core/directives/use_ids';
 import { FormControl } from '@angular/forms';
+import { PortalModule } from '@angular/cdk/portal';
+import { ApplicationFormUiFkt } from '../etc/ui_fkt';
 
 @Component({
   selector: 'app-application-statuses',
-  imports: [DynamicSwapper, UseIDsDir],
+  imports: [DynamicSwapper, UseIDsDir, PortalModule],
   templateUrl: './application-statuses.html',
   styleUrl: './application-statuses.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,15 +17,5 @@ import { FormControl } from '@angular/forms';
 export class ApplicationStatuses extends UseDynamicSwapHk {
   public readonly ctrl: InputSignal<FormControl> = input.required();
 
-  public readonly data: CheckBoxFieldT[] = Array.from(
-    { length: 30 },
-    (_: undefined, i: number) => ({
-      label: i + '',
-      val: i + '',
-      id: v4(),
-      field: '',
-      name: '',
-      type: 'checkbox',
-    })
-  );
+  public readonly fields: CheckBoxFieldT[] = ApplicationFormUiFkt.fieldsBox();
 }
