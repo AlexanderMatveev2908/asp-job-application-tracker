6150192af893b29fde10bcf2ca47e22d74ad41e0
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-28 11:27:00 +0200

    🪾 chnage_mail_2fa => 🛠️ _

--- apps/client/src/layout/page_wrapper/page-wrapper.ts
+++ apps/client/src/layout/page_wrapper/page-wrapper.ts
@@ -1,18 +1,22 @@
 import { SpinPageSsr } from '@/common/components/spins/spin_page_ssr/spin-page-ssr';
+import { RefTemplateT } from '@/common/types/etc';
 import { UseInjCtxHk } from '@/core/hooks/use_inj_ctx';
+import { NgTemplateOutlet } from '@angular/common';
 import {
   AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ContentChild,
   input,
   InputSignal,
   signal,
+  TemplateRef,
   WritableSignal,
 } from '@angular/core';
 
 @Component({
   selector: 'app-page-wrapper',
-  imports: [SpinPageSsr],
+  imports: [SpinPageSsr, NgTemplateOutlet],
   templateUrl: './page-wrapper.html',
   styleUrl: './page-wrapper.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,6 +28,10 @@ export class PageWrapper extends UseInjCtxHk implements AfterViewInit {
   // ? personal props
   public readonly waitClient: InputSignal<boolean> = input.required();
   public readonly isPending: InputSignal<boolean> = input(false);
+
+  // ? children
+  @ContentChild('contentRef', { read: TemplateRef })
+  public contentRef: RefTemplateT;
 
   // ? derived
   public readonly isServer: boolean = this.usePlatform.isServer;
