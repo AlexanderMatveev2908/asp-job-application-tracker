999e35d8a70ec5e98bf1d42039ab44f5f7dd5758
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-29 10:26:23 +0200

    🪾 create_application => 🛠️ _

--- apps/client/src/common/components/forms/form_field_box_lg/form-field-box-lg.ts
+++ apps/client/src/common/components/forms/form_field_box_lg/form-field-box-lg.ts
@@ -1,7 +1,5 @@
-import { Nullable } from '@/common/types/etc';
 import { CheckBoxFieldT } from '@/common/types/forms';
 import { UseFormFieldDir } from '@/core/directives/forms/form_field/0.use_form_field';
-import { ApplicationStatusT } from '@/features/applications/etc/types';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -10,7 +8,9 @@ import {
   input,
   InputSignal,
   OnInit,
+  signal,
   Signal,
+  WritableSignal,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
@@ -29,20 +29,47 @@ export class FormFieldBoxLg implements OnInit {
   public readonly f: InputSignal<CheckBoxFieldT> = input.required();
   public readonly ctrl: InputSignal<FormControl> = input.required();
 
+  // ? local state
+  public readonly wasHover: WritableSignal<boolean> = signal(false);
+
+  public onHover(): void {
+    if (this.isChosen()) this.wasHover.set(true);
+  }
+  public onOut(): void {
+    this.wasHover.set(false);
+  }
+
   // ? derived
+  public readonly isChosen: Signal<boolean> = computed(
+    () => this.useFormFieldDir.val?.() === this.f().val
+  );
   public readonly transform: Signal<string> = computed(
-    () => `scale(${this.useFormFieldDir.val?.() === this.f().val ? '1.15' : '1'})`
+    () => `scale(${this.isChosen() ? '1.2' : '1'})`
   );
   public readonly transformHover: Signal<string> = computed(
-    () => `scale(${this.useFormFieldDir.val?.() === this.f().val ? '1.15' : '1.2'})`
+    () => `scale(${!this.wasHover() ? '1.2' : this.isChosen() ? '1.2' : '1'})`
+  );
+  public readonly bg: Signal<string> = computed(
+    () => `var(--${this.isChosen() ? 'gray__300' : 'neutral__950'})`
+  );
+  public readonly clr: Signal<string> = computed(
+    () => `var(--${this.isChosen() ? 'neutral__950' : 'gray__300'})`
+  );
+  public readonly bgHover: Signal<string> = computed(
+    () =>
+      `var(--${!this.wasHover() ? 'gray__300' : this.isChosen() ? 'gray__300' : 'neutral__950'})`
+  );
+  public readonly clrHover: Signal<string> = computed(
+    () =>
+      `var(--${!this.wasHover() ? 'neutral__950' : this.isChosen() ? 'neutral__950' : 'gray__300'})`
   );
 
   public onClick(): void {
-    const existing: Nullable<ApplicationStatusT> = (this.useFormFieldDir.val?.() ??
-      null) as Nullable<ApplicationStatusT>;
-    const v: ApplicationStatusT = this.f().val as ApplicationStatusT;
+    this.wasHover.set(this.isChosen());
 
-    this.ctrl().setValue(existing === v ? '' : v);
+    this.ctrl().markAsDirty();
+    this.ctrl().markAsTouched();
+    this.ctrl().setValue(this.isChosen() ? '' : this.f().val);
   }
 
   ngOnInit(): void {
