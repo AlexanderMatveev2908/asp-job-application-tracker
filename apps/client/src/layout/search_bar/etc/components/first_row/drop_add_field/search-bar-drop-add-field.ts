e24c40dfc0298fedd6d052de991f3267fc49aaf4
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-10-30 13:14:08 +0200

    🪾 search_bar => 🔥 _

--- apps/client/src/layout/search_bar/etc/components/first_row/drop_add_field/search-bar-drop-add-field.ts
+++ apps/client/src/layout/search_bar/etc/components/first_row/drop_add_field/search-bar-drop-add-field.ts
@@ -1,22 +1,27 @@
-import { Nullable } from '@/common/types/etc';
+import { ElDomT, Nullable, RefDomT } from '@/common/types/etc';
 import { TxtFieldArrayT } from '@/common/types/forms';
 import { BaseSearchBarFormT } from '@/core/paperwork/etc/search_bar';
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
+  HostListener,
   input,
   InputSignal,
+  Signal,
   signal,
   Type,
+  ViewChild,
   WritableSignal,
 } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { SvgStrokeSearchPlus } from '@/common/components/svgs/stroke/search_plus/search-plus';
-import { NgComponentOutlet } from '@angular/common';
+import { NgClass, NgComponentOutlet } from '@angular/common';
+import { v4 } from 'uuid';
 
 @Component({
   selector: 'app-search-bar-drop-add-field',
-  imports: [NgComponentOutlet],
+  imports: [NgComponentOutlet, NgClass],
   templateUrl: './search-bar-drop-add-field.html',
   styleUrl: './search-bar-drop-add-field.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -27,12 +32,62 @@ export class SearchBarDropAddField<T> {
   public readonly formVal: InputSignal<Nullable<BaseSearchBarFormT<T>>> = input.required();
   public readonly txtInputsAvailable: InputSignal<TxtFieldArrayT[]> = input.required();
 
+  // ? derived
+  public readonly txtFieldsLessPresent: Signal<TxtFieldArrayT[]> = computed(() => {
+    const namesIn: Set<string> = new Set<string>(
+      this.formVal()?.txtInputs?.map((f: TxtFieldArrayT) => f.name)
+    );
+    return this.txtInputsAvailable().filter((f: TxtFieldArrayT) => !namesIn.has(f.name));
+  });
+
   // ? local state
   public readonly isOpen: WritableSignal<boolean> = signal(false);
 
   // ? helpers
   public readonly setIsOpen: (v: boolean) => void = (v: boolean) => this.isOpen.set(v);
+  public toggle(): void {
+    this.isOpen.set(!this.isOpen());
+  }
+
+  // ? children
+  @ViewChild('dropRef') dropRef: RefDomT;
+  @ViewChild('dropBtnRef') dropBtnRef: RefDomT;
+
+  // ? derived
+  public readonly twd: Signal<string> = computed(() =>
+    this.isOpen()
+      ? 'opacity-100 translate-y-[120%] pointer-events-auto'
+      : 'opacity-0 translate-y-0 pointer-events-none'
+  );
 
   // ? props span drop-abs
   public readonly SvgDrop: Type<unknown> = SvgStrokeSearchPlus;
+
+  // ? listeners
+
+  public addField(f: TxtFieldArrayT): void {
+    const txtInputs: FormArray = this.form().get('txtInputs') as FormArray;
+
+    txtInputs.push(
+      new FormControl({
+        ...f,
+        id: v4(),
+      })
+    );
+
+    this.setIsOpen(false);
+  }
+
+  @HostListener('document:mousedown', ['$event'])
+  public onMouseDown(e: Event): void {
+    const drop: ElDomT = this.dropRef?.nativeElement;
+    const dropBtn: ElDomT = this.dropBtnRef?.nativeElement;
+
+    const target: Nullable<HTMLElement> = e.target as HTMLElement;
+
+    if ([drop, dropBtn, target].some((el: ElDomT | HTMLElement) => !el)) return;
+
+    if (this.isOpen() && ![drop, dropBtn].some((el: ElDomT) => el!.contains(target)))
+      this.isOpen.set(false);
+  }
 }
