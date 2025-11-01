7b45cf95c480c9a8c1cad66f86b99b3baff7b7b6
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-01 21:06:02 +0200

    🪾 cards_applications => 🛠️ _

--- apps/client/src/pages/job_applications/read_all/application_item/application-item.ts
+++ apps/client/src/pages/job_applications/read_all/application_item/application-item.ts
@@ -3,25 +3,27 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
-  inject,
   input,
   InputSignal,
   Signal,
 } from '@angular/core';
 import { LibCssApplication } from './etc/css';
-import { SpanPropsT } from '@/common/components/els/span/etc/types';
+import { SpanEventPropsT, SpanPropsT } from '@/common/components/els/span/etc/types';
 import { ApplicationItemUiFkt } from './etc/ui_fkt';
 import { WithIdT } from '@/common/types/etc';
 import { Span } from '@/common/components/els/span/span';
-import { DropAbs } from '@/common/components/drop/abs/drop-abs';
-import { UseIDsDir } from '@/core/directives/use_ids';
 import { UseDropHk } from '@/core/hooks/use_drop';
-import { SvgStrokeNotes } from '@/common/components/svgs/stroke/notes/notes';
-import { SvgFillStatus } from '@/common/components/svgs/fill/status/status';
+import { ApplicationStatusRow } from './status_row/application-status-row';
+import { LinkShadow } from '@/common/components/links/link_shadow/link-shadow';
+import { UseSpanDir } from '@/core/directives/use_span';
+import { BtnShadow } from '@/common/components/btns/btn_shadow/btn-shadow';
+import { UseIDsDir } from '@/core/directives/use_ids';
+import { SvgFillFancyPen } from '@/common/components/svgs/fill/fancy_pen/fancy-pen';
+import { SvgStrokeTrash } from '@/common/components/svgs/stroke/trash/trash';
 
 @Component({
   selector: 'app-application-item',
-  imports: [Span, DropAbs, UseIDsDir],
+  imports: [Span, ApplicationStatusRow, LinkShadow, UseSpanDir, BtnShadow, UseIDsDir],
   templateUrl: './application-item.html',
   styleUrl: './application-item.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -29,9 +31,6 @@ import { SvgFillStatus } from '@/common/components/svgs/fill/status/status';
 })
 export class ApplicationItem {
   public readonly application: InputSignal<ApplicationT> = input.required();
-
-  // ? hooks
-  public readonly useDrop: UseDropHk = inject(UseDropHk);
 
   // ? derived
   public readonly cssTheme: Signal<string> = computed(() =>
@@ -42,13 +41,19 @@ export class ApplicationItem {
     ApplicationItemUiFkt.pairsLabelSvg(this.application())
   );
 
-  public readonly spanNotes: SpanPropsT = {
-    label: null,
-    Svg: SvgStrokeNotes,
-  };
+  public readonly spanLink: Signal<SpanEventPropsT> = computed(() => ({
+    eventT: 'INFO',
+    label: 'Update',
+    Svg: SvgFillFancyPen,
+  }));
 
-  public readonly spanStatus: Signal<SpanPropsT> = computed(() => ({
-    label: this.application().status,
-    Svg: SvgFillStatus,
+  public readonly pathLink: Signal<string> = computed(
+    () => `/job-applications/put/${this.application().id}`
+  );
+
+  public readonly deleteSpan: Signal<SpanEventPropsT> = computed(() => ({
+    eventT: 'ERR',
+    label: 'Delete',
+    Svg: SvgStrokeTrash,
   }));
 }
