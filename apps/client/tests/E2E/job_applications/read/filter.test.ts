67e8a619d540043f6a5bcf104dd0ee596633773c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-02 15:03:26 +0200

    🪾 final_tests => 🎨 _

--- apps/client/tests/E2E/job_applications/read/filter.test.ts
+++ apps/client/tests/E2E/job_applications/read/filter.test.ts
@@ -1,12 +1,12 @@
-import test, { Browser, Locator } from '@playwright/test';
-import { preReadApplications } from './pre';
+// import test, { Browser, Locator } from '@playwright/test';
+// import { preReadApplications } from './pre';
 
-test('ok', async ({ browser }: { browser: Browser }) => {
-  const { lib, applications, gridWrapper } = await preReadApplications(browser);
+// test('ok', async ({ browser }: { browser: Browser }) => {
+//   const { lib, applications, gridWrapper } = await preReadApplications(browser);
 
-  const btnOpenFilterBar: Locator = await lib.byIdInPage('open_filter_bar');
-  await btnOpenFilterBar.click();
+//   const btnOpenFilterBar: Locator = await lib.byIdInPage('open_filter_bar');
+//   await btnOpenFilterBar.click();
 
-  const filterBar: Locator = await lib.byIdInPage('filter_bar');
-  const closeBtn: Locator = await lib.byIdIn(filterBar, 'close_filter_bar');
-});
+//   const filterBar: Locator = await lib.byIdInPage('filter_bar');
+//   const closeBtn: Locator = await lib.byIdIn(filterBar, 'close_filter_bar');
+// });
